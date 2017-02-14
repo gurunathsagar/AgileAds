9132ded519a30e51fc849a182135eddf6ebb9804
arakhade <ara.akhilesh@gmail.com>
2017-02-13 20:07:22 -0500

    libraries

--- wavelets.r
+++ wavelets.r
@@ -1,3 +1,14 @@
+load.lib<-function(libT,l=NULL)
+{
+	lib.loc <- l
+	print(lib.loc)
+	
+	if (length(which(installed.packages(lib.loc=lib.loc)[,1]==libT))==0)
+	{
+		install.packages(libT, lib=lib.loc,repos='http://cran.us.r-project.org')
+	}
+}
+
 libraries<-function()
 {
 	load.lib("wavelets")
