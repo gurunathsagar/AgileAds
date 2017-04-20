29722869f71afd1a42990c5e60c9c470e59212ee
Gurunath <gurunath.sagar@gmail.com>
2017-04-20 19:56:32 -0400

    Added polling mechanism file. Not working

---
+++ sockets/pollServer.py
@@ -0,0 +1,148 @@
+from time import sleep
+import socket 
+from threading import Thread 
+from SocketServer import ThreadingMixIn 
+
+class ResourceManager(Thread):
+
+    def __init__(self, connList):
+        Thread.__init__(self)
+        self.connList = connList
+
+    def run(self):
+
+        print "Resource manager spawned"
+        while True:
+
+            if len(connList) == 0:
+                continue
+
+            print "new time", "len of list = ", len(connList)
+
+            resourceValue = [0.0] * len(connList)
+
+            runningThreads = []
+            connListCopy = connList
+            for i in range(len(connListCopy)):
+                newthread = ResourceCollector(connListCopy[i])
+                newthread.start()
+                runningThreads.append(newthread)
+
+            for thread in runningThreads:
+                thread.join()
+
+            for i in range(len(runningThreads)):
+                resourceValue[i] = runningThreads[i].receivedValue
+                print "Inserted Value ", resourceValue[i]
+
+            #for i in range(len(resourceThreads)):
+                #resourceThreads[i].start()
+
+            #for thread in resourceThreads:
+                #thread.join()
+
+            avgResourceVal = 0.0
+
+            for value in resourceValue:
+                print "value being added: ", value
+                avgResourceVal += value
+
+            avgResourceVal /= len(resourceValue)
+
+            print "avg = ", avgResourceVal, " no. of clients open = ", len(connListCopy)
+            sleep(2)
+
+
+class ResourceCollector(Thread):
+
+    def __init__(self, conn):
+        Thread.__init__(self)
+        self.conn = conn
+        # self.i = i
+        self.receivedValue = 0.0
+        # #self.resourceValue = resourceValue
+
+    def run(self):
+        print "Sending ready message"
+        self.conn.send("ready#")
+        print "Sent ready message"
+        recvValue = conn.recv(2048)
+        print "Received value from client as: ", recvValue
+        self.receivedValue = float(recvValue)
+
+
+# # Multithreaded Python server : TCP Server Socket Thread Pool
+# class ClientThread(Thread): 
+ 
+#     def __init__(self,ip,port): 
+#         Thread.__init__(self) 
+#         self.ip = ip 
+#         self.port = port 
+#         print "[+] New server socket thread started for " + ip + ":" + str(port)
+#         fname = ''
+#         file_handle = ''
+        
+#     def run(self): 
+#         data = ''
+#         while True : 
+#             data += conn.recv(2048)
+#             if 'nameofprocess' in data:
+#                 recvBuffer = data.split('#')
+#                 filename = recvBuffer[1:]
+#                 if recvBuffer[-1]:
+#                     data = recvBuffer[-1]
+#                 else:
+#                     data = ''
+#                 #print 'filename is ', filename[0]
+#                 #print 'data: ', data
+#                 fname = filename[0]
+#                 file_handle = open(fname, 'a')
+#                 continue
+#             file_handle = open(fname, 'a')
+#             #file_handle.write(str(data))
+#             recvBuffer = data.split('#')
+#             #print "Recv Buffer as follows: ", recvBuffer
+#             if recvBuffer[-1]:
+#                 data = recvBuffer[-1]
+#             else:
+#                 data = ''
+#             for values in recvBuffer:
+#                 if values:
+#                     print "Server received data:", values, 'fname = ', fname
+#                     file_handle.write(str(values)+"\n")
+#             file_handle.close()
+#             #MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
+#             #if MESSAGE == 'exit':
+#                 #break
+#             #conn.send()  # echo 
+ 
+# Multithreaded Python server : TCP Server Socket Program Stub
+TCP_IP = '0.0.0.0' 
+TCP_PORT = 2000 
+BUFFER_SIZE = 20  # Usually 1024, but we need quick response 
+ 
+tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+tcpServer.bind((TCP_IP, TCP_PORT)) 
+threads = [] 
+resourceThreads = []
+
+connList = []
+newthread = ResourceManager(connList)
+newthread.start()
+threads.append(newthread)
+
+
+while True: 
+    tcpServer.listen(4) 
+    print "Multithreaded Python server : Waiting for connections from TCP clients..." 
+
+    (conn, (ip,port)) = tcpServer.accept() 
+    connList.append(conn)
+    #newthread = ResourceCollector(conn) 
+    #newthread.start() 
+    #threads.append(newthread) 
+    #resourceThreads.append(newthread)
+ 
+for t in threads: 
+    t.join() 
