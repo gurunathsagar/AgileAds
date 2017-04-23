65cf04fde0b12dd3658fe0b5e560d4c1fc4a64ac
arakhade <ara.akhilesh@gmail.com>
2017-04-23 14:47:06 -0400

    code cleanuup

--- waveletTransform.py
+++ waveletTransform.py
@@ -5,21 +5,110 @@ import pandas as pd
 import numpy as np
 import math
 from collections import Counter
+import socket
+import sys
 
-def max_index(stateVector,p):
-	newState = np.random.choice(a=stateVector,p =p)
-	d = {}
-	for i in stateVector:
-		d[i]=p[i]
-	#print "The state vector probablity p is\n",p	
-	#print "The seleceted State is \n",newState
-	#print "The dictionary d is \n",d
-	#print "\n"
-	return newState
+def max_index(arr):
+	index = 0;m = 0
+	n = len(arr)
+	for i in range(n):
+		if arr[i]>m:
+			index = i;
+			m = arr[i]
+	return index
+
+class sloCheck(object):
+	def __init__(self,maxUsage, nTimes):
+		self.sloDefmaxUsage = maxUsage
+		self.sloDefn = nTimes
+		#By definition, SLO is violated if resource usage in a 2 minute(64 units) interval is above maxUsage nTimes
+		self.nTimesSLOundetected=0
+	def isViolated(self, prediction, nlen,actual=None):
+		countP=0;countA=0;
+		for i in range(nlen):
+			if prediction[i]>self.sloDefmaxUsage:
+				countP+=1
+			if actual != None and actual[i]>self.sloDefmaxUsage:	
+				countA+=1
+			if(countP>self.sloDefn):
+				#SLO predicted, return True
+				return True
+			if(countA > self.sloDefn):
+				#SLO was not detected. resulted in actual SLO
+				self.nTimesSLOundetected+=1
+		return False
+		
+
+
+class Accuracy(object):
+	def __init__(self, actualTs, predictedTs):
+		#self.rmse
+		#self.underAllocationPercent = 0
+		self.rmse = 0
+		self.underAllocationPercent = 0
+		self.overAllocationPercent = 0
+
+		error = np.array(predictedTs) - np.array(actual_wValues); n = len(actualTs); sumSquares = 0
+		for e in error:
+			sumSquares += (float(e*e))/n
+			if e < 0:
+				self.underAllocationPercent+=1
+			elif e > 0:
+				self.overAllocationPercent+=1
+		self.rmse = math.sqrt(sumSquares)
+		self.underAllocationPercent = (self.underAllocationPercent/float(n))*100
+		self.overAllocationPercent = (self.overAllocationPercent/float(n))*100
+		pass
+	def getErrors(self):
+		print "rmse:", self.rmse
+		print "overallocation percent:", self.overAllocationPercent
+		print "underallocation percent:", self.underAllocationPercent
+	def getRmse(self):
+		return self.rmse
+	
+		
+
+def predictSimpleMarkovChain(ts, start, end, nV,w, nStates = 40,padPercent=0):
+	#nV = number of values to predict
+	#w -> The array where the predicted values will be appended
+	stateTs = ts[start:end+1];
+	n = end - start + 1; 
+	a_min = min(stateTs); a_max = max(stateTs); a_range = a_max-a_min
+	binSize = (float(a_range+1))/nStates
+	#print "range,n_states,binsize", a_range, nStates,binSize
+	for i in range(len(stateTs)):
+		stateTs[i] = int(math.floor((stateTs[i]-a_min)/binSize))
+	#Caluclation the Transition Matrix
+	p = np.zeros((nStates,nStates))
+	row_sum = np.zeros(nStates)
+	stateVector = range(nStates)
+	for i in range(n-1):
+		p[stateTs[i]][stateTs[i+1]]+=1
+		row_sum[stateTs[i]]+=1
+	for i in range(nStates):
+		row = p[i]
+		s = row_sum[i]
+		if s!=0:
+			for j in range(len(row)):
+				row[j]=row[j]/s			
+		else:
+			for j in range(len(row)):
+				row[j] = 1.0/nStates
+	#predict nV values and append in the array
+	lastState = stateTs[-1]
+	x = np.zeros(nStates)
+	x[lastState]=1;p1=1
+	predictedStates = []; 
+	for i in range(nV):
+		p1 = np.dot(p1,p)
+		x = np.dot(x,p1); k = max_index(x)
+		predictedStates.append(k)
+		demand =a_min + k*binSize + binSize/2
+		w.append(demand + demand*padPercent/100.00)
 
 
 class WaveletTransform(object) :
-	def __init__(self,noScales = None,wavelet = 'haar', d=8192, w = 64,binSize = 2):
+	def __init__(self,noScales = None,wavelet = 'haar', d=8192, w = 64):
 		scales = int(math.log(w,2))
 		if(noScales==None):
 			self.noScales = scales
@@ -28,8 +117,6 @@ class WaveletTransform(object) :
 		self.waveletFunction = wavelet
 		self.d = d
 		self.w = w
-		self.predictionCoeff = []
-		self.binSize = binSize			#size of each bin of markov chain. If total range is 100, then number of states = 100/2.5 = 40
 		if(wavelet == 'haar'):
 			self.factor = 1/math.sqrt(2)
 	def forwardTransformSingleScale(self,ts, n):
@@ -63,142 +150,116 @@ class WaveletTransform(object) :
 		while(i>=0):
 			self.inverseTransformSingleScale(ts,n/(2**i))
 			i-=1
-	def predictSimpleMarkovChain(self,ts, start, end, nV):
-		#nV = number of values to predict
-		#w -> The array where the predicted values must be appended
-		a = ts[start:end+1]; b = a[:]
-		n = end - start + 1; binSize = self.binSize
-		a_min = min(a); a_max = max(a); a_range = a_max-a_min
-		nStates = int(math.ceil((a_range+1)/binSize))
-		if nStates < 40:
-			nStates = 40
-			binSize = int(math.ceil((a_range+1)/nStates))
-		print "range and n_states ", a_range, nStates
-		for i in range(len(a)):
-			a[i] = int(math.floor((a[i]-a_min)/binSize))
-		#Caluclation the Transition Matrix
-		p = np.zeros((nStates,nStates))
-		row_sum = np.zeros(nStates)
-		stateVector = range(nStates)
-		for i in range(n-1):
-			p[a[i]][a[i+1]]+=1
-			row_sum[a[i]]+=1
-		for i in range(nStates):
-			row = p[i]
-			s = row_sum[i]
-			if s!=0:
-				for j in range(len(row)):
-					row[j]=row[j]/s			
-			else:
-				for j in range(len(row)):
-					row[j] = 1.0/nStates
-		#predict nV values and append in the array
-		lastState = a[-1]
-		x = np.zeros(nStates)
-		x[lastState]=1;p1=1
-		predictedCoeffStates = []
-		graph = [];step = self.d/len(a);i = 0;X_AxisScale = [];X_PredictedScale = []
-		while(i<self.d):
-			X_AxisScale.append(i);
-			i+=step
-		for i in range(nV):
-			p1 = np.dot(p1,p)
-			x = np.dot(x,p1)
-			predictedCoeffStates.append(max_index(stateVector,x))
-		i = step
-		for k in predictedCoeffStates:
-			X_PredictedScale.append(self.d + i);i+=step
-			graph.append(a_min + k*binSize + binSize/2)
-			self.predictionCoeff.append(a_min + k*binSize + binSize/2)
-		#print len(a);
-		#print len(X_AxisScale)
-		#print len(graph)
-		#print len(X_PredictedScale)
-		ret = [X_AxisScale,b,X_PredictedScale,graph]
-		#plt.plot(X_AxisScale,b); plt.plot(X_PredictedScale,graph)
-		return ret
-		#print X_PredictedScale
-	def predictCoeffs(self,ts):
-		d = self.d; w= self.w;scaleNo  = self.noScales
+
+	def predictCoeffs(self,ts, ret,algo = 'simplemarkov',padPercent = 0):
+		d = self.d; w= self.w;scaleNo  = self.noScales;
 		i = 0; n = self.d / (2**scaleNo); 
 		nV = int(self.w /(2**scaleNo))
-		ret = self.predictSimpleMarkovChain(ts,i, n-1,nV)
-		#plt.plot(ret[0],ret[1]);plt.plot(ret[2],ret[3])
-		#print "Approx Signal Predicted Value-"
-		#print self.predictionCoeff
+		if algo == 'simplemarkov':
+			predictionAlgo = predictSimpleMarkovChain
+
+		predictionAlgo(ts,i, n-1,nV,ret,padPercent=padPercent)
 		#predict the detailed coefficients
 		while(scaleNo>0):
 			nV = self.w/(2**scaleNo); i +=n;
 			n = self.d/(2**scaleNo);end = i+n-1
-			ret = self.predictSimpleMarkovChain(ts,i,end,nV)
-			#if(scaleNo == 1):
-			#	plt.plot(ret[0],ret[1]);plt.plot(ret[2],ret[3])
-			#print "Detailed signal Scale ",scaleNo
-			#print self.predictionCoeff
+			predictionAlgo(ts,i,end,nV,ret,padPercent=padPercent)
 			scaleNo-=1
-		return self.predictionCoeff
 
 
-filename = '13324.csv'
-#filename = '1805.csv'	
-data = pd.read_csv(filename)
-timeseries = list(data['X'])
-d = 4096; w= 64; haarFactor = 1/math.sqrt(2)
-xAxisRange_w = np.array([i for i in range(d, d+w)])
-xAxisRange_dw = np.array([i for i in range(d+w)])
-
-ts = list(timeseries[:d]);a=ts[:]
-n = len(ts); max_n = max(ts);min_n = min(ts)
-nScales = int(math.log(w,2))
-
-
-haar = WaveletTransform(d=d,w = w,binSize = 1)
-
-haar.forwardTransform(ts=a)
-predicted_wValues = haar.predictCoeffs(ts=a)
-haar.inverseTransform(ts=predicted_wValues)
-actual_wValues = list(timeseries[d:d+w])
-
-#Now for plotting the predicted values vs original values
+class timeseries(object):
+	def __init__(self,lengthTs=0):
+		self.ts = [5]*lengthTs
+		self.lengthTs = lengthTs
+	def updateTs(self, recentData):
+		n = len(recentData);i=0;j=0
+		while i< self.lengthTs - n:
+			self.ts[i]=self.ts[i]+n
+			i+=1
+		while j<n:
+			self.ts[i] = recentData[j]
+			i+=1;j+=1
+	def getTs(self):
+		return self.ts			    	
 
 
-plt.plot(xAxisRange_dw[4000:],timeseries[4000:d+w]) #Plotting the original timeseries wrt timeslots
-rmse_array = np.array(predicted_wValues) - np.array(actual_wValues); n = len(predicted_wValues)
-rmse = sum(np.multiply(rmse_array , rmse_array))
-rmse = sum(np.multiply(rmse_array , rmse_array))
-print math.sqrt(rmse/n)
-#plotting the original values in window w vs predicted values
-plt.plot(xAxisRange_w,actual_wValues)
-plt.plot(xAxisRange_w,predicted_wValues)
-plt.show()
-"""		
-data = pd.read_csv(filename)
-timeseries = list(data['X'])
-d = 4096; w= 64; haarFactor = 1/math.sqrt(2)
-xAxisRange_w = np.array([i for i in range(d, d+w)])
-xAxisRange_dw = np.array([i for i in range(d+w)])
-
-ts = list(timeseries[:d]);a=ts[:]
-n = len(ts); max_n = max(ts);min_n = min(ts)
-nScales = int(math.log(w,2))
 
 
-haar = WaveletTransform(d=d,w = w)
-
-haar.forwardTransform(ts=a)
-predicted_wValues = haar.predictCoeffs(ts=a)
-haar.inverseTransform(ts=predicted_wValues)
-actual_wValues = list(timeseries[d:d+w])
-
-#Now for plotting the predicted values vs original values
 
 
-plt.plot(xAxisRange_dw[4000:],timeseries[4000:d+w]) #Plotting the original timeseries wrt timeslots
 
-#plotting the original values in window w vs predicted values
-plt.plot(xAxisRange_w,actual_wValues)
-plt.plot(xAxisRange_w,predicted_wValues)
-plt.show()"""
-#for i in b:
-#	print i
+
+
+
+
+
+d = 4096; w=64; haarFactor = 1/math.sqrt(2)
+ts = timeseries(lengthTs=d);n = ts.lengthTs
+
+
+"""
+Socket Server Program 
+http://www.binarytides.com/python-socket-programming-tutorial/
+"""
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+print 'Socket bind complete'
+
+
+while True:
+	s.listen(10)
+	print 'Socket now listening'
+
+	conn, addr = s.accept()
+	#display client information
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	data = ''
+	while True:
+		data += conn.recv(2048)
+		if '*' in data:
+			conn.close()
+			break
+
+	"""
+		The prediction Module gets the data. Close the TCP socket and process the data.
+		The first integer n represents how many resource usages are sent
+	"""
+	print data
+	data = data.split('#')
+	n = int(data[0])
+	recentData = []
+	for i in range(1,n+1):
+		recentData.append(float(data[i]))			
+	
+	ts.updateTs(recentData)
+	#tsPredict0 = timeseries()
+	tsPredict5 = []
+	tsList = ts.ts[:];max_n = max(tsList);min_n = min(tsList)
+	haar = WaveletTransform(d=d,w = w)
+
+	haar.forwardTransform(ts=tsList);
+	#haar.predictCoeffs(ts=tsList,ret=tsPredict0,padPercent=0)
+	haar.predictCoeffs(ts=tsList,ret=tsPredict5,padPercent=5)
+
+	#haar.inverseTransform(ts=tsPredict0)
+	haar.inverseTransform(ts=tsPredict5)
+
+	slo = sloCheck(maxUsage=75, nTimes=10)
+	isSloViolated = slo.isViolated(tsPredict5,len(tsPredict5))
+	if(isSloViolated):
+		#Trigger the VM Cloning
+		pass
+	
+	#plot Resource Usage vs Prediction
+
+	
+
 
