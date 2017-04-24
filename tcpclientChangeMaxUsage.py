f5413d1d9038a7aab53511451299d66e682facf6
arakhade <ara.akhilesh@gmail.com>
2017-04-24 09:34:19 -0400

    public port

---
+++ tcpclientChangeMaxUsage.py
@@ -0,0 +1,18 @@
+from time import sleep
+import socket 
+from SocketServer import ThreadingMixIn 
+
+PredictionPort = 8080
+PredictionHost = '152.46.17.1'
+
+sendString = '30#15#*' 
+tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+	status = tcpClientA.connect_ex((PredictionHost, PredictionPort))
+	tcpClientA.send(sendString)
+
+except socket.error, exc:
+	print "Error Connecting to Prediction Module. Not sending this value"
+
+tcpClientA.close()
+
