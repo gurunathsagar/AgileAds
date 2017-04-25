40a15117833bf404e20d3afff883990da23a1df3
arakhade <ara.akhilesh@gmail.com>
2017-04-25 08:08:16 -0400

    slave scaling monitor

--- slaveServerPoolManager.py
+++ slaveServerPoolManager.py
@@ -8,6 +8,15 @@ TCP_PORT = 9000
 
 BUFFER_SIZE = 1024
 
+hostDict = {"host2":{"user":"arakhade",
+						"ip":"152.46.16.115"
+					},
+			"host1":{
+						"user":"ghanams",
+						"ip":"152.1.13.88"
+					}
+			}
+
 tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 tcpServer.bind((TCP_IP, TCP_PORT)) 
@@ -23,6 +32,6 @@ while True:
 	print data
 	data = data.split('#')
 	vmName = data[0]
-	username = "arakhade"
-	serverIp = "152.46.16.115"
+	username = hostDict['host2']['user']
+	serverIp = hostDict['host2']['ip']
 	call(['bash', 'run.sh',vmName, username, serverIp])
